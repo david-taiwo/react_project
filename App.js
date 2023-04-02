6d99da8d05e2d29a2c4b750b13790a3259df9506
david-taiwo <87903025+david-taiwo@users.noreply.github.com>
2023-04-02 05:31:14 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./app.css";
+import Home from "./Components/Home/Home.jsx"
+import Navbar from "./Components/Navbar/Navbar.jsx"
+import Main from "./Components/Main/Main.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Main/>
+      <Footer/>
+
+    </>
+  )
+};
+
+export default App;
